c51399bf165e0c3c745922cbace85a6ac71ca592
Anton Skrypii <thetonytekk@gmail.com>
2022-01-27 04:38:31 +0200

    Add sections

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,45 @@
 
 $(document).ready(function () {
     console.log('Start');
+/*
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 50;
+    var headerHeight = $('header').outerHeight();
+    var header = $('header');
+
+    window.addEventListener('scroll', function() {
+        didScroll = true;
+    });
+
+    setInterval(function() {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+
+        // Make sure they scroll more than delta
+        if (Math.abs(lastScrollTop - st) <= delta) return;
+
+        if (st > lastScrollTop && st > headerHeight){
+
+            header.css({ 'top': '-80px' });
+        } else {
+            // Scroll Up
+            if(st + $(window).height() < $(document).height()) {
+                header.css({
+                    'top': '0' ,
+                //    'background-color':'rgba(0, 0, 0, 0.8)'
+                });
+            }
+        }
+
+        lastScrollTop = st;
+    }
+    */
 });
 
