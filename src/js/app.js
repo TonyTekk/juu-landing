596d0cb85ede56f78e47abbe28bfc28212bf76cd
Anton Skrypii <thetonytekk@gmail.com>
2022-01-28 17:14:49 +0200

    Add build

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,23 @@
 $(document).ready(function() {
+    var audio = document.getElementById("audio");
+
+    $('#btn_play').on('click', function(event) {
+        event.preventDefault();
+        $('#btn_pause').css('display', 'block');
+        $('#btn_play').css('display', 'none');
+        audio.play();
+    });
+
+    $('#btn_pause').on('click', function(event) {
+        event.preventDefault();
+        $('#btn_play').css('display', 'block');
+        $('#btn_pause').css('display', 'none');
+        audio.pause();
+    });
+
+});
+
+$(document).ready(function() {
     $(window).scroll( function(){
         $('.__fadeIn').each( function(i){
 
@@ -13,16 +32,13 @@ $(document).ready(function() {
 });
 
 $(document).ready(function () {
-
     var didScroll;
     var lastScrollTop = 0;
     var delta = 50;
     var headerHeight = $('header').outerHeight();
     var header = $('header');
 
-    window.addEventListener('scroll', function() {
-        didScroll = true;
-    });
+    window.addEventListener('scroll', function() { didScroll = true; });
 
     setInterval(function() {
         if (didScroll) {
@@ -33,12 +49,10 @@ $(document).ready(function () {
 
     function hasScrolled() {
         var st = $(this).scrollTop();
-
         // Make sure they scroll more than delta
         if (Math.abs(lastScrollTop - st) <= delta) return;
 
         if (st > lastScrollTop && st > headerHeight){
-
             header.css({ 'top': '-80px' });
         } else {
             // Scroll Up
@@ -54,3 +68,36 @@ $(document).ready(function () {
     }
 });
 
+$(document).ready(function () {
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 50;
+    var headerHeight = $('footer').outerHeight();
+    var header = $('footer');
+
+    window.addEventListener('scroll', function() { didScroll = true; });
+
+    setInterval(function() {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+        // Make sure they scroll more than delta
+        if (Math.abs(lastScrollTop - st) <= delta) return;
+
+        if (st > lastScrollTop && st > headerHeight){
+            header.css({ 'bottom': '-80px' });
+        } else {
+            // Scroll Up
+            if(st + $(window).height() < $(document).height()) {
+                header.css({ 'bottom': '0' });
+            }
+        }
+
+        lastScrollTop = st;
+    }
+});
